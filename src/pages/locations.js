22a805ab31aeb110f550e586b4f0104065ca98bb
Nicolas Pierre-Fanfan <nicolas@oxylabs.io>
2021-01-23 12:34:38 +0200

    add cities query to location component to fix build error

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -1,10 +1,9 @@
 import Layout from "components/layout";
 import { StyledH1 } from "components/styledComponents";
-import { Link } from "gatsby";
+import { Link, useStaticQuery } from "gatsby";
 import React from "react";
 import { commonMsgs } from "utils/commonMsgs";
 import { CSS_ALIGN, CSS_DISPLAY, CSS_FLEX_DIRECTION } from "utils/theme";
-import { useContentfulCities } from "./contentful";
 
 const locationsList = {
   display: CSS_DISPLAY.FLEX,
@@ -12,15 +11,38 @@ const locationsList = {
   flexDirection: CSS_FLEX_DIRECTION.COLUMN,
 }
 
+
+const useAllContentfulCity = () => {
+  const { allContentfulCity } = useStaticQuery(graphql`
+    query MyQuery {
+      allContentfulCity {
+        edges {
+          node {
+            name
+            description
+            location {
+              lat
+              lon
+            }
+            gatsbyPath(filePath: "/city/{contentfulCity.name}")
+          }
+        }
+      }
+    }
+  `
+  );
+  return allContentfulCity.edges;
+}
+
 const LocationPage = () => {
-  const { edges } = useContentfulCities();
+  const cities = useAllContentfulCity();
 
   return (
     <Layout>
       <StyledH1>{commonMsgs.findUs}</StyledH1>
       <ul style={locationsList}>
       {
-        edges.map(({node}) => (
+        cities.map(({node}) => (
           <li key={node.name}>
             <Link to={node.gatsbyPath}>{node.name}</Link>
           </li>
